6b34b711ae11035c38900c5e42cfc18613c568d2
Leonir <leonir.ad@gmail.com>
2021-04-23 06:33:44 -0300

    padronização do apib e uso do include

---
+++ MockAPI/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    sourceFiles: 'api_blueprint.apib',
+    debugMode: true,
+    discover: true,
+    watch: true,
+    serverPort: 4000,
+    public: false,
+	delay: 0000,
+    ignoreHeader: ['Accept', 'Content-Type']
+};
